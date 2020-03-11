8ff0ee939bbfb30aabdd781b64a139211dc247de
keuchguerian <a_guimaraes21@yahoo.com.br>
2020-03-11 00:19:39 +0000

    Versao1

---
+++ index.js
@@ -0,0 +1,23 @@
+const server = required ('express');
+
+const server = express();
+
+const tarefas = [
+    {
+        id: 1,
+        descricao: "Comprar Pão",
+        finalizado: false
+    }
+];
+
+server.get('/tarefas', function(request, response)
+    {
+        return response.json(tarefas);
+})
+
+
+
+
+
+
+server.listen(process.env.PORT || 3000);
